b6ceadb045945f14e765421fa311a332bc30416c
Carrillo Jimenez <carrilg9@nala.roche.com>
2024-08-10 15:50:54 -0600

    init app

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p className={styles.title}>
+          Opinion
+        </p>
+        
+      </div>
+      
+    </main>
+  );
+}

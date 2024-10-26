d7ec0d5a39837a1b32caaeedd393d71a2eae0cda
Carrillo Jimenez <carrilg9@nala.roche.com>
2024-10-26 13:40:14 -0600

    Corrige configuración ESM en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
-// next.config.js
-module.exports = {
-    reactStrictMode: true,
-    trailingSlash: true, // Esto asegura que las rutas sean manejadas con un '/' al final
+// next.config.mjs
+export default {
+    reactStrictMode: true,  // Habilita el modo estricto de React para ayudar a identificar problemas potenciales
+    trailingSlash: true,    // Asegura que todas las rutas sean manejadas con un '/' al final
+  
+    // Configuramos redirecciones automáticas
     async redirects() {
       return [
         {
-          source: '/',
-          destination: '/login', // Redirigimos la página principal al login
-          permanent: true,
+          source: '/',          // Ruta raíz de la aplicación
+          destination: '/login', // Redirigimos la raíz a la página de login
+          permanent: true,       // Indicamos que esta redirección es permanente (HTTP 308)
         },
       ];
     },

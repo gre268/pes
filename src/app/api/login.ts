e9fe59a9bfbe673fb5c537aa261ce9e0ca6e606a
gre268 <gre268@gmail.com>
2024-10-26 17:09:04 -0600

    login hardcode

--- src/app/api/login.ts
+++ src/app/api/login.ts
@@ -4,34 +4,30 @@ import mysql from 'mysql2/promise';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
-    console.log('Recibido usuario:', username, 'Contraseña:', password);  // Log para verificar si los datos están llegando correctamente
 
     try {
-      console.log('Intentando conectar a la base de datos...'); // Log antes de la conexión
-      // Conectamos a la base de datos
+      // Temporalmente, hardcodeamos las credenciales para verificar la conexión
       const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: Number(process.env.DB_PORT) || 3306,
+        host: 'opinionwebsite.cdogwouyu9yy.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: '123456789',
+        database: 'opinionwebsite',
+        port: 3306,
       });
       console.log('Conexión exitosa a la base de datos'); // Log para confirmar la conexión a la base de datos
 
-      // Ejecutamos la consulta
+      // Ejecutamos la consulta para buscar el usuario
       const [rows] = await connection.execute(
         'SELECT * FROM user WHERE userName = ? AND password = ?',
         [username, password]
       );
-      console.log('Consulta ejecutada, filas recibidas:', rows); // Log para verificar los resultados de la consulta
+      console.log('Consulta ejecutada, filas:', rows); // Log para verificar los resultados de la consulta
 
       if ((rows as any[]).length > 0) {
         const user = (rows as any[])[0];
         const role = user.role_ID === 1 ? 'admin' : 'regular';
-        console.log('Usuario encontrado, Rol:', role); // Log para confirmar el usuario y rol encontrado
         res.status(200).json({ success: true, role });
       } else {
-        console.log('Credenciales incorrectas'); // Log si las credenciales no son válidas
         res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
       }
 
@@ -47,7 +43,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     }
   } else {
-    console.log('Método no permitido:', req.method); // Log si el método no es POST
     res.status(405).json({ message: 'Método no permitido' });
   }
 }
